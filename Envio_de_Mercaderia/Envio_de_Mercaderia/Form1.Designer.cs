043f3ade692a478f157db1e4ccbebf61e651d047
Allan-Sanchez <asanchezrixtun@gmail.com>
2017-09-04 14:44:30 -0600

    envio_mercaderias
    programa grafico que controla el envio de mercaderia

---
+++ Envio_de_Mercaderia/Envio_de_Mercaderia/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Envio_de_Mercaderia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtSi = new System.Windows.Forms.RadioButton();
+            this.rbtNo = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtEfectivo = new System.Windows.Forms.RadioButton();
+            this.rbtTarjeta = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Peso Mercancia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "&Valor Mercancia";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(123, 35);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(189, 20);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(123, 69);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(189, 20);
+            this.txtValor.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtNo);
+            this.groupBox1.Controls.Add(this.rbtSi);
+            this.groupBox1.Location = new System.Drawing.Point(36, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(81, 66);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "&Es lunes";
+            // 
+            // rbtSi
+            // 
+            this.rbtSi.AutoSize = true;
+            this.rbtSi.Location = new System.Drawing.Point(17, 19);
+            this.rbtSi.Name = "rbtSi";
+            this.rbtSi.Size = new System.Drawing.Size(34, 17);
+            this.rbtSi.TabIndex = 0;
+            this.rbtSi.Text = "&Si";
+            this.rbtSi.UseVisualStyleBackColor = true;
+            // 
+            // rbtNo
+            // 
+            this.rbtNo.AutoSize = true;
+            this.rbtNo.Checked = true;
+            this.rbtNo.Location = new System.Drawing.Point(17, 42);
+            this.rbtNo.Name = "rbtNo";
+            this.rbtNo.Size = new System.Drawing.Size(39, 17);
+            this.rbtNo.TabIndex = 1;
+            this.rbtNo.TabStop = true;
+            this.rbtNo.Text = "&No";
+            this.rbtNo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtTarjeta);
+            this.groupBox2.Controls.Add(this.rbtEfectivo);
+            this.groupBox2.Location = new System.Drawing.Point(161, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(105, 87);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "&Tipo de Pago";
+            // 
+            // rbtEfectivo
+            // 
+            this.rbtEfectivo.AutoSize = true;
+            this.rbtEfectivo.Location = new System.Drawing.Point(26, 49);
+            this.rbtEfectivo.Name = "rbtEfectivo";
+            this.rbtEfectivo.Size = new System.Drawing.Size(64, 17);
+            this.rbtEfectivo.TabIndex = 1;
+            this.rbtEfectivo.Text = "E&fectivo";
+            this.rbtEfectivo.UseVisualStyleBackColor = true;
+            // 
+            // rbtTarjeta
+            // 
+            this.rbtTarjeta.AutoSize = true;
+            this.rbtTarjeta.Location = new System.Drawing.Point(26, 26);
+            this.rbtTarjeta.Name = "rbtTarjeta";
+            this.rbtTarjeta.Size = new System.Drawing.Size(58, 17);
+            this.rbtTarjeta.TabIndex = 2;
+            this.rbtTarjeta.Text = "T&arjeta";
+            this.rbtTarjeta.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(53, 191);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(64, 39);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 261);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Envio de Mercaderia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtNo;
+        private System.Windows.Forms.RadioButton rbtSi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtTarjeta;
+        private System.Windows.Forms.RadioButton rbtEfectivo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Envio_de_Mercaderia/Envio_de_Mercaderia/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Envio_de_Mercaderia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (txtPeso.Text == "") 
+            {
+                MessageBox.Show("Debes ingresar un peso de la mercancia: ");
+                txtPeso.Focus();
+                return;
+            }
+            decimal peso;
+            try
+            {
+                peso = Convert.ToDecimal(txtPeso.Text);
+
+            }
+            catch (Exception)
+            {
+
+                peso = 0;
+            }
+            if (peso <= 0)
+            {
+                MessageBox.Show("Debes ingresar un peso de la mercaderia mayor a cero : ","Erro");
+                txtPeso.Focus();
+                txtPeso.Text = "";
+                
+                return;
+            
+            }
+            if (txtValor.Text == "")
+            {
+                MessageBox.Show("Debes ingresar un valor valido: ");
+                txtValor.Focus();
+                return;
+            }
+            decimal valor;
+            try
+            {
+                valor = Convert.ToDecimal(txtValor.Text);
+
+            }
+            catch (Exception)
+            {
+
+                valor = 0;
+            }
+            if (valor <= 0)
+            {
+                MessageBox.Show("Debes ingresar un valor mayor a cero : ", "Erro");
+                txtValor.Focus();
+                txtValor.Text = "";
+
+                return;
+
+            }
+
+            decimal tarifa = CalcularTarifa(peso, valor);
+            decimal descuento = CalcularDescuento(peso, tarifa);
+            decimal promocion = CalcularPromocion(rbtSi.Checked, rbtEfectivo.Checked,valor,tarifa);
+
+            string mensaje;
+            mensaje = string.Format("Tarifa: {0:N2}", tarifa);
+
+            if (promocion > 0)
+            {
+
+                mensaje += string.Format("\nPromocion: {0:N2}", promocion);
+                mensaje += string.Format("\nTotal: {0:N2}", tarifa - promocion);
+
+                
+            }
+            else
+            {
+                mensaje += string.Format("\nDescuento: {0:N2}", descuento);
+                mensaje += string.Format("\nTotal: {0:N2}", tarifa - descuento);
+                
+            }
+            MessageBox.Show(mensaje,"Listo");
+    
+
+        }
+
+
+        
+
+
+
+        private decimal CalcularPromocion(bool esLunes, bool esEfectivo, decimal valor, decimal tarifa)
+        {
+            if (esLunes && !esEfectivo) return tarifa * 0.50M;
+            if (esEfectivo && valor > 1000000) return tarifa * 0.40M;
+            return 0;
+        }
+
+        
+
+        private decimal CalcularDescuento(decimal valor, decimal tarifa)
+        {
+            if (valor < 300000) return 0;
+            if (valor <= 600000) return tarifa * 0.10M;
+            if (valor <= 1000000) return tarifa * 0.20M;
+            return tarifa * 0.30M;
+        }
+
+        private decimal CalcularTarifa(decimal peso, decimal valor)
+        {
+
+            if (peso < 100) return 20000;
+            if (peso <= 150) return 25000;
+            if (peso <= 200) return 30000;
+            return 35000 +(int) ((peso - 200) / 10) * 2000;
+        }
+    }
+}
